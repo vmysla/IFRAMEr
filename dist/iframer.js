1b4c33ab26e5893b996ab85a4faff34854828d3b
Vlad Mysla <vlad@pearl.com>
2015-08-04 01:05:48 +0300

    iframe.document.close() added. GA task creates proxy from now.

--- dist/iframer.js
+++ dist/iframer.js
@@ -10,37 +10,38 @@
 
         if( script.type == 'widget/html' ) {
 
-        var parent  = script.parentNode || document;
-        var next    = script.nextElementSibling || script.nextSibling;
+          var parent  = script.parentNode || document;
+          var next    = script.nextElementSibling || script.nextSibling;
 
-        var iframe  = document.createElement( 'iframe' );
-        var iscript = (next.type == 'widget/javascript') ? ('<script>' + next.innerHTML + '</'+'script>') : '';
-        var ihtml   = script.innerHTML + iscript;
+          var iframe  = document.createElement( 'iframe' );
+          var iscript = (next.type == 'widget/javascript') ? ('<script>' + next.innerHTML + '</'+'script>') : '';
+          var ihtml   = script.innerHTML.replace(/(<script.*)(\/>)/ig,'$1></script>') + iscript;
 
-        iframe.width  = '0';  
-        iframe.height = '0';
+          iframe.width  = '0';  
+          iframe.height = '0';
+          iframe.frameborder = '0';
 
-        parent.insertBefore( iframe, script );
-        parent.removeChild( script );
-        if(iscript) parent.removeChild( next );
+          parent.insertBefore( iframe, script );
+          parent.removeChild( script );
+          if(iscript) parent.removeChild( next );
 
-        var iwindow   = iframe.contentWindow;
-        var idocument = iframe.contentDocument || iwindow.document;
+          var iwindow   = iframe.contentWindow;
+          var idocument = iframe.contentDocument || iwindow.document;
 
-        idocument.open();
+          idocument.open();
 
-        for( var taskName in tasks ){  
-            try {
-                tasks[taskName]( iframe, iwindow, idocument );
-            } catch( error ) {
-                if( window.console && console.log ) {
-                    console.log( 'iframit task exception', taskName, script.name, error.message );
-                }
-            }
-        }
-        
-        idocument.write( ihtml );
-               
+          for( var taskName in tasks ){  
+              try {
+                  tasks[taskName]( iframe, iwindow, idocument );
+              } catch( error ) {
+                  if( window.console && console.log ) {
+                      console.log( 'IFRAMEr task exception', taskName, script.name, error.message );
+                  }
+              }
+          }
+          
+          idocument.write( ihtml );
+          idocument.close();       
         }
 
     }
@@ -60,15 +61,11 @@
       var gaTrackerName = window['GoogleAnalyticsObject'] || gaDefaultName;
       var gaTracker = window[gaTrackerName];
 
-      if(!gaTracker) return;
-
-      function share(){  
-          iwindow[gaDefaultName] = gaTracker; 
-      }
-
-      // share existing tracker immediately and once more time after it was loaded:
-      share();
-      gaTracker(share); 
+      iwindow[gaDefaultName] = function(){
+        var gaTrackerName = window['GoogleAnalyticsObject'] || gaDefaultName;
+        var gaTracker = window[gaTrackerName];
+        if(gaTracker) gaTracker.apply(window, arguments);
+      }; 
       
     }
 
