d77392554b26edf17dcc54cf833981fecc2a9369
DefiantDog66 <43454318+def14nt@users.noreply.github.com>
2018-10-10 17:23:22 +0100

    Update iframer.js
    Added better annotation and beautified it

--- dist/iframer.js
+++ dist/iframer.js
@@ -1,81 +1,96 @@
-
-// frameless document.querySelectorAll: 
-// https://gist.github.com/vmysla/ed1daad8109ea1ba811e
-
-// frameless jQuery: 
-// https://gist.github.com/vmysla/00806f5c98b51857ee36
-
-
-var outside = (typeof module == 'undefined' || !module.exports) 
-	  ? (typeof window != 'undefined' && window || this)
-	  : module.exports;
-
-outside.iframer = iframer;
-
-
-/*
-    // ------------ Usage: -------------
-    // =================================
-    
+/**
+ * @fileoverview
+ * Frameless jQuery:
+ * https://gist.github.com/vmysla/00806f5c98b51857ee36
+ * @example
+ * ```js
     var container = document.querySelector('body');
-    var html      = "<h1>Timestamp <script>document.write( Date.now() );</script></h1>";
+    var html = '<h1>Timestamp <script>document.write( Date.now() );</script></h1>';
 
-    function logFrameEvent(frame, eventName){
+    function logFrameEvent(frame, eventName) {
         console.log(
-            "eventName"      , eventName, 
-            "frame.resolved" , frame.resolved, 
-            "document.body"  , frame.document && frame.document.body 
+            'eventName', eventName, 
+            'frame.resolved' , frame.resolved, 
+            'document.body'  , frame.document && frame.document.body 
         );
     }
 
     var listeners = {
-        'onBeforeAppend'    : logFrameEvent,
-        'onAfterAppend'     : logFrameEvent,
-        'onBeforePopulate'  : logFrameEvent,
-        'onAfterPopulate'   : logFrameEvent
+        'onBeforeAppend': logFrameEvent,
+        'onAfterAppend' : logFrameEvent,
+        'onBeforePopulate': logFrameEvent,
+        'onAfterPopulate': logFrameEvent
+    };
+    ```
+ */
+
+// Capture the scope for exporting modules.
+var outside = (function () {
+    if(typeof module === 'object' && module.exports) {
+        return module;
+    }
+
+    if(typeof window === 'object') {
+        return window;
+    }
+
+    return this;
+})();
+
+outside.iframer = iframer;
+
+/**
+ * @function iframer
+ * @returns {void}
+ */
+function iframer(container, html, listeners, domain) {
+    /**
+     * @type object
+     */
+    var frame = {
+        container: container || document.body,
+        html: html || '',
+        listeners: listeners || {},
+        domain: domain || document.domain,
+
+        element: document.createElement('iframe'),
+        resolved: false,
+        document: false,
+        window: false
     };
 
-    // --------------- API: -------------
-
-    iframer(container, html, listeners);
-
-    // ----------------------------------
-
-*/
-
-function iframer(container, html,  listeners, domain){
-
-    var frame = {
-        container : container || document.body,
-        html      : html      || '',
-        listeners : listeners || {},
-        domain    : domain    || document.domain,
-
-        element   : document.createElement('iframe'),
-        resolved  : false,
-
-        document  : false,
-        window    : false
-    }
-
     iframer.append(frame);
-    iframer.resolve(frame) 
-        ? iframer.populate(frame) 
-        : iframer.populate(frame, document.domain);
-}
-
-iframer.append = function (frame){
-    iframer.trigger(frame, 'onBeforeAppend');
-    frame.container.appendChild( frame.element );
-    iframer.trigger(frame, 'onAfterAppend');
-}
-
-iframer.populate = function (frame, domain) {
-    if(domain){
-        frame.element.populate = iframer.populate;
-        frame.element.src = 'javascript:(document.open().domain="'+domain+'") && frameElement.populate(frameElement)';
+    if(iframer.resolve(frame)) {
+        iframer.populate(frame);
+    } else {
+        iframer.populate(frame, document.domain);
     }
-    else {
+}
+
+/**
+ * @function append
+ * @name iframer.append
+ * @returns {void}
+ */
+iframer.append = function(frame) {
+    iframer.trigger(frame, 'onBeforeAppend');
+    frame.container.appendChild(frame.element);
+    iframer.trigger(frame, 'onAfterAppend');
+};
+
+/**
+ * @function populate
+ * @name iframer.populate
+ * @returns {void}
+ */
+iframer.populate = function(frame, domain) {
+    if (domain) {
+        frame.element.populate = iframer.populate;
+        frame.element.src =
+            'javascript:(document.open().domain="' +
+            domain +
+            '") && frameElement.populate(frameElement)';
+    } else {
         iframer.resolve(frame);
         iframer.trigger(frame, 'onBeforePopulate');
         frame.element.frameElement = frame.element;
@@ -83,19 +98,29 @@ iframer.populate = function (frame, domain) {
         frame.document.close();
         iframer.trigger(frame, 'onAfterPopulate');
     }
-}
+};
 
-iframer.trigger = function (frame, eventName){
+/**
+ * @function trigger
+ * @name iframer.trigger
+ * @returns {boolean}
+ */
+iframer.trigger = function(frame, eventName) {
     var handler = frame.listeners[eventName];
     return handler && handler(frame, eventName);
-}
+};
 
-iframer.resolve = function (frame){
+/**
+ * @function resolve
+ * @name iframer.resolve
+ * @returns {!boolean}
+ */
+iframer.resolve = function(frame) {
     try {
-      frame.window = frame.element.contentWindow;
-      frame.document = frame.element.contentDocument || (frame.window && frame.window.document);
-    } catch(ex) {
-      // mute
-    };
-    return frame.resolved = !!(frame.window && frame.document);
-}
+        frame.window = frame.element.contentWindow;
+        frame.document =
+            frame.element.contentDocument ||
+            (frame.window && frame.window.document);
+    } catch (ex) {}
+    return (frame.resolved = !!(frame.window && frame.document));
+};
